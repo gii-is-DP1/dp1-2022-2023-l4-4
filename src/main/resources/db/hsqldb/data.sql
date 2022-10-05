7f3a7072ddf47c439218636a768df684bb2105eb
virolialo <sowonation.gfriend@gmail.com>
2022-10-05 21:21:48 +0200

    Merge branch 'virolialo' of https://github.com/gii-is-DP1/dp1-2022-2023-l4-4.git into virolialo

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -43,7 +43,7 @@ INSERT INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085
 INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683', 'owner1');
 INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435', 'owner1');
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
-INSERT INTO owners VALUES (13, 'Virgilio', 'Oliva Alonso', 'Telemaco, 3', 'Sevilla', '6085555487', 'owner1');
+INSERT INTO owners VALUES (13, 'Virgilio', 'Oliva Alonso', 'Telemaco, 3', 'Sevilla', '661064697', 'owner1');
 
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (2, 'Basil', '2012-08-06', 6, 2);
