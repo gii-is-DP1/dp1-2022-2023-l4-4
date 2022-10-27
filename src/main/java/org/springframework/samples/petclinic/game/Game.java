3f1d20fefb68b148acba714cd7e717a4735cbe45
rafael <rafaelgonzalezcastillero@gmail.com>
2022-10-27 12:35:44 +0200

    added game, suspecttype

--- src/main/java/org/springframework/samples/petclinic/game/Game.java
+++ src/main/java/org/springframework/samples/petclinic/game/Game.java
@@ -1,5 +1,48 @@
 package org.springframework.samples.petclinic.game;
 
+import java.time.Duration;
+import java.time.LocalDate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.springframework.samples.petclinic.enumerates.SuspectType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "games")
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique = true, nullable = false, precision = 10)
+	private long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+	private User user;
+
+    private Integer playersNumber;
+    private boolean isPrivate;
+    private SuspectType initialPlayer;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+	private User winner;
+
+    private Duration duration;
+    private Integer round;
+    private SuspectType playerTurn;
     
 }
