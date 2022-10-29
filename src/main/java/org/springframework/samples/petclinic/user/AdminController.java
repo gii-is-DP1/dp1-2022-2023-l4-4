1f00e46336a2cdad9fa13ed61227405e5bdc4610
darkgigi <daniel.arriaza.arriaza@gmail.com>
2022-10-29 17:54:38 +0200

    Creación de entidad, servicios y controlador

---
+++ src/main/java/org/springframework/samples/petclinic/user/AdminController.java
@@ -0,0 +1,8 @@
+package org.springframework.samples.petclinic.user;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminController {
+    
+}
---
+++ src/main/java/org/springframework/samples/petclinic/user/SuspectType.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.petclinic.user;
+
+public enum SuspectType {
+    YELLOW,
+    BLUE,
+    WHITE,
+    PURPLE,
+    RED,
+    GREEN
+}
