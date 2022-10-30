ba4ec3e78d1fbfbdabd411515d3b128d7dc57f05
ismbargar <ismbargar@alum.us.es>
2022-10-30 12:57:22 +0100

    Code cleaned

--- src/main/java/org/springframework/cluedo/pet/Pet.java
+++ src/main/java/org/springframework/cluedo/pet/Pet.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.cluedo.pet;
+package org.springframework.samples.petclinic.pet;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -33,9 +33,9 @@ import javax.persistence.Table;
 
 import org.springframework.beans.support.MutableSortDefinition;
 import org.springframework.beans.support.PropertyComparator;
-import org.springframework.cluedo.model.NamedEntity;
-import org.springframework.cluedo.owner.Owner;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.NamedEntity;
+import org.springframework.samples.petclinic.owner.Owner;
 
 /**
  * Simple business object representing a pet.
