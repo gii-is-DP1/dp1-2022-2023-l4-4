24fe91d61ce51c071ec513efc4089c27e00850fe
rafael <rafaelgonzalezcastillero@gmail.com>
2022-10-30 13:38:35 +0100

    fix for merge

--- src/main/java/org/springframework/cluedo/game/GameService.java
+++ src/main/java/org/springframework/cluedo/game/GameService.java
@@ -2,11 +2,21 @@ package org.springframework.cluedo.game;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.transaction.annotation.Transactional;
+
 public class GameService {
-    
+
     private GameRepository gameRepository;
 
-    public List<Game> findGames() {
+    @Autowired
+	public GameService(GameRepository gameRepository) {
+		this.gameRepository = gameRepository;
+	}
+    
+    @Transactional(readOnly = true)
+	public List<Game> findAllGames() throws DataAccessException {
 		return gameRepository.findAll();
-    }
+	}
 }
