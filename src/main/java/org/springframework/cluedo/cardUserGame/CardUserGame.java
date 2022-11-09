3530962d198bcaddb1eac7f42d90f20ea40aa1d2
ismbargar <ismbargar@alum.us.es>
2022-11-09 17:29:49 +0100

    CardUserGame

--- src/main/java/org/springframework/cluedo/cardUserGame/CardUserGame.java
+++ src/main/java/org/springframework/cluedo/cardUserGame/CardUserGame.java
@@ -1,25 +1,27 @@
 package org.springframework.cluedo.cardUserGame;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.cluedo.card.Card;
 import org.springframework.cluedo.model.BaseEntity;
-
+import org.springframework.cluedo.user.UserGame;
 
 @Entity
-@Table(name = "cardUserGames")
+@Table(name = "userGameCards")
 public class CardUserGame extends BaseEntity{
-
-    @Column(name = "card_id")
-    @NotNull
-    private Integer CardId;
     
-    @Column(name = "user_game_id")
     @NotNull
-    private Integer UserGameId;
+    @ManyToOne
+    @JoinColumn(name = "card_id")
+    private Card Card;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_game_id")
+    private UserGame UserGame;
 
 
 }
