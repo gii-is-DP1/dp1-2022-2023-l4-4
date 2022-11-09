2d21adb8f3051fa9a3837cc5ec5284e70c0439f9
ismbargar <ismbargar@alum.us.es>
2022-11-09 17:29:35 +0100

    Status param added

--- src/main/java/org/springframework/cluedo/game/Game.java
+++ src/main/java/org/springframework/cluedo/game/Game.java
@@ -2,17 +2,15 @@ package org.springframework.cluedo.game;
 
 import java.time.Duration;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.springframework.cluedo.enumerates.SuspectType;
+import org.springframework.cluedo.model.BaseEntity;
 import org.springframework.cluedo.user.User;
+import org.springframework.cluedo.enumerates.Status;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -21,23 +19,19 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(name = "games")
-public class Game {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(unique = true, nullable = false, precision = 10)
-	private long id;
+public class Game extends BaseEntity{
 
     @OneToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "host_id")
 	private User host;
 
     private Integer playersNumber;
     private boolean isPrivate;
+    private Status status;
     private SuspectType initialPlayer;
 
     @OneToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "winner_id")
 	private User winner;
 
     private Duration duration;
