bebce27d8f295557294109fd122d5a52533fcd74
rafael <rafaelgonzalezcastillero@gmail.com>
2022-11-09 18:33:47 +0100

    accusation class fixed

---
+++ src/main/java/org/springframework/cluedo/accusation/Accusation.java
@@ -0,0 +1,34 @@
+package org.springframework.cluedo.accusation;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.cluedo.card.Card;
+import org.springframework.cluedo.model.BaseEntity;
+import org.springframework.cluedo.turn.Turn;
+
+public class Accusation extends BaseEntity{
+
+    @OneToOne
+    @JoinColumn(name = "turn_id")
+    @NotNull
+    private Turn turn;
+
+    @ManyToOne
+    @JoinColumn(name = "suspect_card_id")
+    @NotNull
+    private Card suspectCard;
+
+    @ManyToOne
+    @JoinColumn(name = "weapon_card_id")
+    @NotNull
+    private Card weaponCard;
+
+    @ManyToOne
+    @JoinColumn(name = "room_card_id")
+    @NotNull
+    private Card roomCard;
+    
+}
