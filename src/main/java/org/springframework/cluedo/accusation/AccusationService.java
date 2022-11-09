0be5acc123d54f95878e5ff47a6f8e12c7675dbc
rafael <rafaelgonzalezcastillero@gmail.com>
2022-11-09 18:36:31 +0100

    accusation repository and service created

---
+++ src/main/java/org/springframework/cluedo/accusation/AccusationService.java
@@ -0,0 +1,14 @@
+package org.springframework.cluedo.accusation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccusationService {
+    private AccusationRepository accusationRepository;
+
+    @Autowired
+    public AccusationService(AccusationRepository accusationRepository) {
+        this.accusationRepository = accusationRepository;
+    }
+}
