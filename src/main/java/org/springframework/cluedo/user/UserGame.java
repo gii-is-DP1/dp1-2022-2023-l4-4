511a048ce943f77fd8ecc00e74410ac796ce4ecc
darkgigi <daniel.arriaza.arriaza@gmail.com>
2022-11-09 18:51:32 +0100

    Historias 10,11,12 y 13

--- src/main/java/org/springframework/cluedo/user/UserGame.java
+++ src/main/java/org/springframework/cluedo/user/UserGame.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cluedo.game.Game;
 import org.springframework.cluedo.model.BaseEntity;
 
 import lombok.Getter;
@@ -15,10 +16,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserGame extends BaseEntity{
-    @NotNull
-    private Integer gameId;
-    @NotNull
-    private Integer userId;
     @Min(0)
     @Value("${some.key:0}")
     private Integer accusationsNumber;
@@ -30,4 +27,6 @@ public class UserGame extends BaseEntity{
     @ManyToOne(optional = false)
     private User user;
 
+    @ManyToOne
+    private Game game;
 }
