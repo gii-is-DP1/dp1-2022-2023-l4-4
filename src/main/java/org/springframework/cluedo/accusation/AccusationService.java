c8f6402c3c3334c05a6ea1edbcedc956d06ffc52
rafael <rafaelgonzalezcastillero@gmail.com>
2022-11-10 14:14:16 +0100

    getMatchingCardsFromUser

--- src/main/java/org/springframework/cluedo/accusation/AccusationService.java
+++ src/main/java/org/springframework/cluedo/accusation/AccusationService.java
@@ -1,6 +1,11 @@
 package org.springframework.cluedo.accusation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cluedo.card.Card;
+import org.springframework.cluedo.user.UserGame;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,4 +20,19 @@ public class AccusationService {
     public Accusation saveAccusation(Accusation accusation) {
         return accusationRepository.save(accusation);
     }
+
+    public List<Card> getMatchingCardsFromUser(Accusation accusation, UserGame user) {
+        List<Card> matchingCards = new ArrayList<Card>();
+        Card roomCard = accusation.getRoomCard();
+        Card suspectCard = accusation.getSuspectCard();
+        Card weaponCard = accusation.getWeaponCard();
+        
+        for(Card card:user.getCards()) {
+            if(card.equals(weaponCard) || card.equals(roomCard) || card.equals(suspectCard)) {
+                matchingCards.add(card);
+            }
+        }
+        return matchingCards;
+    }
+
 }
