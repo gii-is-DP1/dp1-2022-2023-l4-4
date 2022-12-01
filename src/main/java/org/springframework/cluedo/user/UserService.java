76f0c214dbb8bf296ab07b544971a530dfa1a1e1
ismbargar <ismbargar@alum.us.es>
2022-12-01 18:34:28 +0100

    Create game fixed
    Co-authored-by: Rafael González Castillero <rafgoncas1@users.noreply.github.com>

--- src/main/java/org/springframework/cluedo/user/UserService.java
+++ src/main/java/org/springframework/cluedo/user/UserService.java
@@ -4,15 +4,16 @@ package org.springframework.cluedo.user;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cluedo.card.Card;
 import org.springframework.cluedo.enumerates.SuspectType;
 import org.springframework.cluedo.game.Game;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -62,7 +63,6 @@ public class UserService {
     public void initializePlayers(List<User> lobby, Game copy) { 
 		
 		List<SuspectType> suspects= new ArrayList<>(Arrays.asList(SuspectType.values()));
-		
 		for (User user : lobby) {
 			Integer available = suspects.size();
 			UserGame userGame = new UserGame();
@@ -73,7 +73,7 @@ public class UserService {
 			Integer randomInt = ThreadLocalRandom.current().nextInt(available);
 			userGame.setSuspect(suspects.get(randomInt));
 			suspects.remove(suspects.get(randomInt));
-			System.out.println("A"); 
+			userGame.setCards(new HashSet<Card>());
 			userGameService.saveUserGame(userGame);
 			copy.addPlayers(userGame);
 		}
