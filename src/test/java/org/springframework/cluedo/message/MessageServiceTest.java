3d723dc681314903cbd366e2ebb0c34140a165ae
ismbargar <ismbargar@alum.us.es>
2023-01-09 16:59:07 +0100

    +

--- src/test/java/org/springframework/cluedo/message/MessageServiceTest.java
+++ src/test/java/org/springframework/cluedo/message/MessageServiceTest.java
@@ -1,33 +1,21 @@
 package org.springframework.cluedo.message;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.when;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cluedo.card.CardService;
 import org.springframework.cluedo.enumerates.Status;
 import org.springframework.cluedo.exceptions.DataNotFound;
 import org.springframework.cluedo.game.Game;
 import org.springframework.cluedo.user.User;
-import org.springframework.cluedo.user.UserGame;
-import org.springframework.cluedo.user.UserService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
