6c8218f2e872d61b3e603b9dd875287ffb26a8c8
josejavier <josalcsan1@alum.us.es>
2023-01-12 12:35:54 +0100

    Test Notification Controller
    Co-authored-by: Daniel Arriaza Arriaza <darkgigi@users.noreply.github.com>

--- src/main/java/org/springframework/cluedo/game/GameController.java
+++ src/main/java/org/springframework/cluedo/game/GameController.java
@@ -797,12 +797,10 @@ public class GameController {
         }
         ModelAndView result=checkPhase(game,Phase.FINAL); 
         if (result==null){
-            System.out.println("ENTRA AQUI");
             gameService.finishTurn(game);
             return new ModelAndView("redirect:/games/"+game.getId()+"/play");
 
         }
-        System.out.println("PASA POR AQUI");
         return result;
     }
     
